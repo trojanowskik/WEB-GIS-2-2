e7337e672d80bda934f38937729ddd93a80f1589
trojanowskik <trojanowskik@wp.pl>
2022-01-26 19:46:15 +0100

    main

--- strona/script/script.js
+++ strona/script/script.js
@@ -8,10 +8,14 @@ require([
     'esri/renderers/visualVariables/VisualVariable',
     'esri/renderers/visualVariables/SizeVariable',
     'esri/widgets/Legend',
+    "esri/widgets/LayerList",
 
-  ], (Map, SceneView,FeatureLayer,Graphic, GraphicsLayer, SimpleRenderer, VisualVariable, SizeVariable, Legend)=>{
+  ], (Map, SceneView,FeatureLayer,Graphic, GraphicsLayer, SimpleRenderer, VisualVariable, SizeVariable, Legend, LayerList)=>{
 
   const fl = new FeatureLayer({
+      url: "https://services.arcgis.com/ue9rwulIoeLEI9bj/ArcGIS/rest/services/Earthquakes/FeatureServer/0"
+  });
+  const fl2 = new FeatureLayer({
       url: "https://services.arcgis.com/ue9rwulIoeLEI9bj/ArcGIS/rest/services/Earthquakes/FeatureServer/0"
     
   });
@@ -20,7 +24,7 @@ require([
 
   const map1 = new Map({
     basemap: "topo-vector",
-    layers: [fl,gl]
+    layers: [fl2, gl]
   });
 
   const view = new SceneView({
@@ -34,7 +38,7 @@ require([
     view: view,
     layerInfos: [
       {
-        layer: fl
+        layer: fl2
       }
     ]
     
@@ -42,37 +46,8 @@ require([
 
   view.ui.add(legend, {position: "bottom-right"});
 
-  /*const geom = {
-    type: "point",
-    longitude: 25,
-    latitude: 52
-  };
-
-  const symbol = {
-    type: "simple-marker",
-    style: "x",
-    color: "red",
-    size: 14,
-    outline: {
-        color: "red"
-    }
-  };
-
-  const attr = {
-    name: "Polska",
-    code: "POL"
-  };
-
-  const graph = new Graphic({
-    geometry: geom,
-    symbol: symbol,
-    attributes: attr,
-  });*/
-/*
-  gl.add(graph);
-  
-  let query = fl.createQuery();
-  query.where = "MAGNITUDE < '4' " ;
+ let query = fl.createQuery();
+  query.where = "MAGNITUDE > 4";
   query.outFields =  ["*"];
   query.returnGeometry = true;
 
@@ -86,70 +61,79 @@ require([
     })
 
   const getResults = (features) => {
-    const symbol = {
-        type: "simple-marker",
-        color: "red",
-        size: 8
+    let symbol = {
+      type: 'simple-marker',
+      size: 15,
+      color: "green",
+      style: "circle"
     };
-
+    
     features.map(elem => {
-        elem.symbol = symbol
+      elem.symbol = symbol;
     });
 
     gl.addMany(features);
-}
-*/
-  const simpleRenderer ={
-    type: 'simple',
-    symbol: {
-        type: 'point-3d',
-        symbolLayers:[
-          {
-            type:'object',
-            resource:{
-              primitive: 'cylinder'
-            },
-            width: 25000
-          }
-        ]
-        
-    },
-    visualVariables: [
-      {
-        type:"color",
-        field:"MAGNITUDE",
-        stops:[
-          {
-            value: 0.5 ,
-            color:"green",
-          },
-          {
-            value: 1.12998071359691,
-            color:"yellow",
-          },
-          {
-            value: 4.48,
-            color:"red",
-          }
-        ]
+
+    const simpleRenderer ={
+      type: 'simple',
+      symbol: {
+          type: 'point-3d',
+          symbolLayers:[
+            {
+              type:'object',
+              resource:{
+                primitive: 'cylinder'
+              },
+              width: 25000
+            }
+          ]
+          
       },
-      {
-        type: 'size',
-        field: 'DEPTH',
-        stops:[
-          {
-            value: -3.39,
-            size: 20000
-          },
-          {
-            value: 30.97,
-            size: 120000
-          }
-        ]
-      }
-    ]
+      visualVariables: [
+        {
+          type:"color",
+          field:"MAGNITUDE",
+          stops:[
+            {
+              value: 0.5 ,
+              color:"green",
+            },
+            {
+              value: 1.12998071359691,
+              color:"yellow",
+            },
+            {
+              value: 4.48,
+              color:"red",
+            }
+          ]
+        },
+        {
+          type: 'size',
+          field: 'DEPTH',
+          stops:[
+            {
+              value: -3.39,
+              size: 20000
+            },
+            {
+              value: 30.97,
+              size: 120000
+            }
+          ]
+        }
+      ]
+
   }
 
-  fl.renderer = simpleRenderer;
+  fl2.renderer = simpleRenderer;
+
+}
+
+const layerlist = new LayerList({
+  view: view,
+}); 
+  
+view.ui.add(layerlist, "bottom-left")
 
 });
